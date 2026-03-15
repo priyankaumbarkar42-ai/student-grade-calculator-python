6c47623e5269bb132b923a4684f06d554bf6b733
priyankaumbarkar42-ai <priyankaumbarkar42@gmail.com>
2026-03-15 15:48:13 +0530

    Add files via upload

---
+++ student_grade_calculator.py
@@ -0,0 +1,24 @@
+
+# Student Grade Calculator
+
+name = input("Enter student name: ")
+
+sub1 = int(input("Enter marks of Subject 1: "))
+sub2 = int(input("Enter marks of Subject 2: "))
+sub3 = int(input("enter marks of Subject 3: "))
+
+total = sub1 + sub2 + sub3 
+average = total / 3
+
+print("\nStudent Name:", name)
+print("Total Marks:", total)
+print("Average:", average)
+
+if average >= 90:
+    print("Grade: A")
+elif average >= 75:
+    print("Grade: B")   
+elif average >= 60:
+    print("Grade: c")
+else:
+    print("Grade: Fail")     
