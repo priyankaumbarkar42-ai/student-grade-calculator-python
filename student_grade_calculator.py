f4b1c31b25771959b7b6f412bb98c77281e77c23
priyankaumbarkar42-ai <priyankaumbarkar42@gmail.com>
2026-03-16 13:38:24 +0530

    Update student_grade_calculator.py

--- student_grade_calculator.py
+++ student_grade_calculator.py
@@ -5,7 +5,7 @@ name = input("Enter student name: ")
 
 sub1 = int(input("Enter marks of Subject 1: "))
 sub2 = int(input("Enter marks of Subject 2: "))
-sub3 = int(input("enter marks of Subject 3: "))
+sub3 = int(input("Enter marks of Subject 3: "))
 
 total = sub1 + sub2 + sub3 
 average = total / 3
@@ -19,6 +19,6 @@ if average >= 90:
 elif average >= 75:
     print("Grade: B")   
 elif average >= 60:
-    print("Grade: c")
+    print("Grade: C")
 else:
     print("Grade: Fail")     
